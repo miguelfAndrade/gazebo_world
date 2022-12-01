1e301465cf2ca880cd70fba328321e36664ca65f
miguelfAndrade <miguelandrade.96.18@gmail.com>
2022-12-01 15:23:22 +0000

    Added new export file and point cloud calcalus

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ World for gazebo robot simulator, which contains a lidar sensor that rotates at 
 
 ### Steps (Only works on linux)
 - Install Gazebo - https://classic.gazebosim.org/tutorials?tut=install_ubuntu&cat=install
-- Install the development package `sudo apt install libgazebo8-dev`
+- Install the development package `sudo apt install libgazebo11-dev`
 - Create a new directory in the root of the project named **build** by running the command `mkdir build`
 - Inside the **build** directory run `cmake ..` `make`
 - Run the command `./vel.sh` in the root of the project
--- vel_data.cc
+++ vel_data.cc
@@ -3,8 +3,11 @@
 #include <gazebo/msgs/msgs.hh>
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
 
-using namespace std;
 
 // Gazebo's API has changed between major releases. These changes are
 // accounted for with #if..#endif blocks in this file.
@@ -14,8 +17,15 @@ using namespace std;
 #include <gazebo/gazebo_client.hh>
 #endif
 
+using namespace std;
+
+#define PI 3.14159265
+#define N 3
+
+void multiplyVector(double mat1[][N], double mat2[N], double res[N]);
 
 string body_tags = ""; //all repeated tags are added in this global string variable
+string points_coordinates = "";
 
 int counter = 0;
 
@@ -64,10 +74,84 @@ void cb(ConstLaserScanStampedPtr &_msg)
     string ranges_tags = "";
     string intensities_tags = "";
 
+
+    double min_angle = 0;
+    double step_angle = scan.angle_step();
+
+    double q0 = orientation.w();
+    double q1 = orientation.x();
+    double q2 = orientation.y();
+    double q3 = orientation.z();
+
+    double m11 = 2*((q0*q0) + (q1*q1))-1;
+    double m12 = 2*((q1*q2) - (q0*q3));
+    double m13 = 2*((q1*q3) + (q0*q2));
+    double m21 = 2*((q1*q2) + (q0*q3));
+    double m22 = 2*((q0*q0) + (q2*q2))-1;
+    double m23 = 2*((q2*q3) - (q0*q1));
+    double m31 = 2*((q1*q3) - (q0*q2));
+    double m32 = 2*((q2*q3) + (q0*q1));
+    double m33 = 2*((q0*q0) + (q3*q3))-1;
+
+    if(scan.angle_min() < 0)
+    {
+        min_angle = (PI/2) + abs(scan.angle_min());
+    }
+    else
+    {
+        min_angle = (PI/2) - scan.angle_min();
+    }
+
+    double R[N][N] = { { m11, m12, m13 },
+                       { m21, m22, m23 },
+                       { m31, m32, m33 }};
+
+    // double angle = 2*acos(orientation.w());
+    // double vertical_angle = orientation.z()/sin(angle/2);
+    
+    // if(orientation.w() == 1.0)
+    // {
+    //     angle = 0;
+    //     vertical_angle = 0;
+    // }
+
+    // cos_a  = W;
+    // sin_a  = sqrt( 1.0 - cos_a * cos_a );
+    // angle  = acos( cos_a ) * 2;
+    // if ( fabs( sin_angle ) < 0.0005 ) sin_a = 1;
+    // tx = X / sin_a;
+    // ty = Y / sin_a;
+    // tz = Z / sin_a;
+    // latitude = -asin( ty );
+    // if ( tx * tx + tz * tz < 0.0005 )
+    //   longitude   = 0;
+    // else
+    //    longitude  = atan2( tx, tz );
+    // if ( longitude < 0 )
+    //   longitude += 360.0;
+
     while(i<ray_total_count)
     {
         ranges_tags += "\t\t<ranges>" + to_string(scan.ranges(i)) + "</ranges>\n";
         intensities_tags += "\t\t<intensities>" + to_string(scan.intensities(i)) + "</intensities>\n";
+
+        double x1 = scan.ranges(i)*sin(min_angle)*cos(0);
+        double y1 = scan.ranges(i)*sin(min_angle)*sin(0);
+        double z1 = scan.ranges(i)*cos(min_angle);
+
+        double v[N]={x1,y1,z1};
+
+        double res[N];
+
+        multiplyVector(R, v, res);
+
+        double x = res[0] + position.x();
+        double y = res[1] + position.y();
+        double z = res[2] + position.z();
+        
+        points_coordinates += to_string(x) + " " + to_string(y) + " " + to_string(z) + "\n";
+        min_angle -= step_angle;
+
         i += 1;
     }
 
@@ -80,19 +164,55 @@ void cb(ConstLaserScanStampedPtr &_msg)
     file.open("../sensor_data/sensor_data.xml"); //open is the method of ofstream
     file << xmlheader + body_tags + xml_final_tag;
     file.close();
+
+    ofstream pointCloud;
+    // pointCloud.open("../sensor_data/sensor_data.xml", fstream::app);
+    pointCloud.open("../sensor_data/sensor_data.xyz"); //open is the method of ofstream
+    pointCloud << points_coordinates;
+    pointCloud.close();
+
+}
+
+// Multiplication of matrices
+// void multiply(double mat1[][N], double mat2[N][], double res[N][])
+// {
+//     int i, j, k;
+//     for (i = 0; i < N; i++) {
+//         for (j = 0; j < N; j++) {
+//             res[i][j] = 0;
+//             for (k = 0; k < N; k++)
+//                 res[i][j] += mat1[i][k] * mat2[k][j];
+//         }
+//     }
+// }
+
+void multiplyVector(double mat1[][N], double mat2[N], double res[N])
+{
+    int i, j;
+    for (int i=0;i<N;i++){
+        for (int j=0;j<N;j++){
+            res[i]+=(mat1[i][j]*mat2[j]);
+        }
+    }
 }
 
 /////////////////////////////////////////////////
 int main(int _argc, char **_argv)
 {
 
-    // if(isdigit(to_string(_argv[1])) == false)
+    // if(!isdigit(to_string(atoi(_argv[0]))))
     // {
-    //     cout << "Please enter a valid number as an argument for max_counter variable!!";
+    //     cout << "\nPlease enter a valid number as an argument for max_counter variable!!\n\n";
     //     return 0;
     // }
-    // int max_counter = (int) _argv[1];
-    int max_counter = 200;
+
+    // int max_counter = atoi(_argv[0]);
+
+    // for (int i = 0; i < _argc; ++i) {
+    //   cout << _argv[i] << endl;
+    // }
+
+    //int max_counter = 200;
     
     // Load gazebo as a client
     #if GAZEBO_MAJOR_VERSION < 6
@@ -108,7 +228,7 @@ int main(int _argc, char **_argv)
     // Publish to the  velodyne topic
     gazebo::transport::SubscriberPtr sub = node->Subscribe("~/my_velodyne/velodyne_hdl-32/top/sensor/scan", cb);
 
-    while (counter < max_counter)
+    while (true)
         gazebo::common::Time::MSleep(10);
 
     // Make sure to shut everything down.
